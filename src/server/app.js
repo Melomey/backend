a3d11b91141a1df711b4b3084268c5e069b2f2e5
Melomey <samuelmelomey@gmail.com>
2024-04-03 15:03:02 +0000

    first commit

---
+++ src/server/app.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+
+dotenv.config()
+const app = express()
+
+app.use(express.json())
+// use router 
+
+const port = process.env.PORT || 4002;
+// const mongoURI = process.env.MONGO_URI
+
+
+
+app.listen(port,() => {
+    console.log(`Express App is Running on ${port}`);
+})
