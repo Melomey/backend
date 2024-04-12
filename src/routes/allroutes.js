97fb46e9af84ab14f6ca0769158007c903303b34
jeniferagbeli <agbelij12@gmail.com>
2024-04-12 15:55:57 +0000

    the routes endpoint

--- src/routes/allroutes.js
+++ src/routes/allroutes.js
@@ -7,18 +7,18 @@ import { authenticateToken } from "../middlewares/auth.middleware.js";
 export const router = Router()
 
 //users
-router.post('/user', addUser)
+router.post('/users', addUser)
 router.post('/login', loginUser)
 router.post('/logout', authenticateToken, logoutUser);
 
-router.get('/user', authenticateToken, getAllUsers)
-router.get('/user/:id', authenticateToken, getSpecificUser)
+router.get('/users', authenticateToken, getAllUsers)
+router.get('/users/:id', authenticateToken, getSpecificUser)
 
 //requests
-router.post('/request', authenticateToken, addRequest)
+router.post('/requests', authenticateToken, addRequest)
 
-router.get('/request', authenticateToken, findRequests)
-router.get('/request/:id', authenticateToken, getSpecificRequest)
+router.get('/requests', authenticateToken, findRequests)
+router.get('/requests/:id', authenticateToken, getSpecificRequest)
 
 
 
