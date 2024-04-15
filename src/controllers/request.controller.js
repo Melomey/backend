539d68605b97b7581d21f60b4a0d538ce75d039d
Melomey <samuelmelomey@gmail.com>
2024-04-15 09:45:17 +0000

    edited add request controller

--- src/controllers/request.controller.js
+++ src/controllers/request.controller.js
@@ -8,7 +8,7 @@ export const addRequest = async (req, res) => {
         const addRequest = await requestModel.create({ ...data, userId: req.user._id })
         console.log(data)
         res.json(addRequest)
-
+        res.status(201).json({message: 'Form submitted successfully'});
     } catch (error) {
         console.log(error)
         res.status(404).json({ message: "Failed to get request" });
@@ -23,7 +23,7 @@ export const findRequests = async (req, res) => {
         const findRequests = await requestModel.find(data)
         console.log(data)
         res.json(findRequests)
-
+        res.status(201).json({message: 'Form submitted successfully'});
     } catch (error) {
         console.log(error)
         res.status(404).json({ message: "Failed to get request" });
@@ -37,7 +37,7 @@ export const getSpecificRequest = async (req, res) => {
         const getSpecificRequest = await requestModel.findById(req.params.id)
         console.log(getSpecificRequest)
         res.json(getSpecificRequest)
-
+        res.status(201).json({message: 'Form submitted successfully'});
     } catch (error) {
         console.log(error)
         res.status(404).json({ message: "Failed to get request" });
