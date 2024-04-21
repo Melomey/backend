f3a574b71e6250fa0ccdb959137b575e6493447b
ellajectey <emmanuella.jecteyasare@gmail.com>
2024-04-21 15:03:48 +0000

    updated all requests controller

--- src/controllers/request.controller.js
+++ src/controllers/request.controller.js
@@ -30,22 +30,38 @@ export const allRequests = async (req, res) => {
 }
 
 //search for requests
-export const findRequests = async (req, res) => {
-  try {
-    const data = req.params.fullName;
-    console.log('data', data)
-    const findRequests = await requestModel
-      .find({fullName:data})
-      .populate("userId", ["_id", "firstName", "lastName", "email"]);
-    console.log('request',findRequests);
-    res.json(findRequests);
-  } catch (error) {
-    console.log(error);
-    res.status(404).json({ message: "Failed to get request" });
-  }
-};
+// export const findRequests = async (req, res) => {
+//   try {
+//     const data = req.params.fullName;
+//     console.log('data', data)
+//     const findRequests = await requestModel
+//       .find({fullName:data})
+//       .populate("userId", ["_id", "firstName", "lastName", "email"]);
+//     console.log('request',findRequests);
+//     res.json(findRequests);
+//   } catch (error) {
+//     console.log(error);
+//     res.status(404).json({ message: "Failed to get request" });
+//   }
+// };
 
 //get a specific request
+
+//get all requests  
+export const findRequests = async (req, res) => {
+    try {
+      const data = req.body;
+      const findRequests = await requestModel
+        .find(data)
+        .populate("userId", ["_id", "firstName", "lastName", "email"]);
+      console.log(data);
+      res.json(findRequests);
+    } catch (error) {
+      console.log(error);
+      res.status(404).json({ message: "Failed to get request" });
+    }
+  };
+  
 export const getSpecificRequest = async (req, res) => {
   try {
     const getSpecificRequest = await requestModel.findById(req.params.id);
