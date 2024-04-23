0b12b672a755ac7fda4246e59a3f509f1479b9a6
ellajectey <emmanuella.jecteyasare@gmail.com>
2024-04-23 12:58:25 +0000

    fixed user requests issue

--- src/controllers/request.controller.js
+++ src/controllers/request.controller.js
@@ -15,7 +15,20 @@ export const addRequest = async (req, res) => {
     res.status(404).json({ message: "Failed to get request" });
   }
 };
-
+//get user's requests 
+export const findUserRequests = async (req, res) => {
+  try {
+    const {email} = req.body;
+    const findRequests = await requestModel
+      .find({email})
+      .populate("userId", ["_id", "firstName", "lastName", "email"]);
+    console.log(data);
+    res.json(findRequests);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ message: "Failed to get request" });
+  }
+};
 //get all requests  
 export const findRequests = async (req, res) => {
   try {
--- src/routes/allroutes.js
+++ src/routes/allroutes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { addUser, getAllUsers, getSpecificUser, loginUser, logoutUser, updateUser } from "../controllers/users.controller.js";
-import { addRequest, findRequests, getSpecificRequest, } from "../controllers/request.controller.js";
+import { addRequest, findRequests,findUserRequests, getSpecificRequest, } from "../controllers/request.controller.js";
 import { authenticateToken } from "../middlewares/auth.middleware.js";
 
 
@@ -18,7 +18,7 @@ router.get('/users/:id', authenticateToken, getSpecificUser)
 //requests
 router.post('/requests', authenticateToken, addRequest)
 router.get('/partner-requests',  findRequests)
-router.get('/requests', authenticateToken, findRequests)
+router.get('/requests', authenticateToken, findUserRequests)
 router.get('/requests/:id', authenticateToken, getSpecificRequest)
 
 
